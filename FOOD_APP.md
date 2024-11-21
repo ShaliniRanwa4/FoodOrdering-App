7f35fe75bac6af7b7aaba9b1a09491b7e33bb8be
ShaliniRanwa4 <ranwashalini4@gmail.com>
2024-11-21 14:50:06 +0530

    Initial commit

---
+++ FOOD_APP.md
@@ -0,0 +1,19 @@
+Layout
+
+App Container
+Header
+
+Logo
+Nav Items
+Body
+
+Search Bar
+Restaurant List
+Restaurant Card
+Image
+Name
+Cusines
+Cost For Two
+Rating
+Expected Delivery Time
+Footer
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+
+    return (
+        <div>
+            <h1>
+                Hi, This is shalini
+            </h1>
+            <h3>
+                 JavaScript | NodeJS | ReactJS | Typescript | 
+            </h3>
+        </div>
+    )
+}
+
+export default About;
